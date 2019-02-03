8abc5d7b586cb385fb43451131588716c9c4727c
Jeremy Mikkola <jeremy@jeremymikkola.com>
2019-02-02 20:46:21 -0800

    Add a sphere

--- src/main.rs
+++ src/main.rs
@@ -42,8 +42,8 @@ impl Vec3 {
         self * Self::new_uniform(val)
     }
 
-    fn dot(self, v2: Self) -> Self {
-        Self::new(self.x * v2.x, self.y * v2.y, self.z * v2.z)
+    fn dot(self, v2: Self) -> f64 {
+        self.x * v2.x + self.y * v2.y + self.z * v2.z
     }
 
     fn cross(self, v2: Self) -> Self {
@@ -113,14 +113,28 @@ impl Ray {
 }
 
 fn color(r: Ray) -> Vec3 {
+    let center = Vec3::new(0.0, 0.0, -1.0);
+    let radius = 0.5;
+    if hit_sphere(center, radius, r) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().make_unit_vector();
     let t = 0.5 * (unit_direction.y + 1.0);
     Vec3::unit_vector(1.0).mul(1.0 - t) + Vec3::new(0.5, 0.7, 1.0).mul(t)
 }
 
+fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    discriminant > 0.0
+}
+
 
 fn main() {
-    let nx = 300;
+    let nx = 200;
     let ny = 100;
 
     println!("P3");
