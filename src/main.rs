bd120b311267c44f4ca858d19ffccda32e49f18d
Jeremy Mikkola <jeremy@jeremymikkola.com>
2019-02-02 20:02:49 -0800

    Start vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    fn squared_length(self) -> f64 {
+        self.x * self.x +
+            self.y * self.y +
+            self.z * self.z
+    }
+
+    fn length(self) -> f64 {
+        self.squared_length().sqrt()
+    }
+
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3 { x: x, y: y, z: z }
+    }
+}
+
+
 fn main() {
     let nx = 200;
     let ny = 100;
@@ -6,12 +29,10 @@ fn main() {
     println!("255");
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r = i as f64 / nx as f64;
-            let g = j as f64 / nx as f64;
-            let b = 0.2;
-            let ir = (255.99 * r) as i64;
-            let ig = (255.99 * g) as i64;
-            let ib = (255.99 * b) as i64;
+            let col = Vec3::new(i as f64 / nx as f64, j as f64 / nx as f64, 0.2);
+            let ir = (255.99 * col.x) as i64;
+            let ig = (255.99 * col.y) as i64;
+            let ib = (255.99 * col.z) as i64;
             println!("{} {} {}", ir, ig, ib);
         }
     }
