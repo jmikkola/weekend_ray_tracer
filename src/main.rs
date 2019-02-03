51699505b5c76fca5ff4c83afe24eca95ab6a985
Jeremy Mikkola <jeremy@jeremymikkola.com>
2019-02-02 21:23:57 -0800

    Multiple spheres

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::f64;
 use std::ops;
 
 #[derive(Copy, Clone)]
@@ -40,6 +41,10 @@ impl Vec3 {
 
     fn mul(self, val: f64) -> Self {
         self * Self::new_uniform(val)
+    }
+
+    fn div(self, val: f64) -> Self {
+        self / Self::new_uniform(val)
     }
 
     fn dot(self, v2: Self) -> f64 {
@@ -112,32 +117,89 @@ impl Ray {
     }
 }
 
-fn color(r: Ray) -> Vec3 {
-    let center = Vec3::new(0.0, 0.0, -1.0);
-    let radius = 0.5;
-    let t = hit_sphere(center, radius, r);
-    if t > 0.0 {
-        let n = (r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0)).make_unit_vector();
-        return Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0).mul(0.5);
-    }
-    let unit_direction = r.direction().make_unit_vector();
-    let t = 0.5 * (unit_direction.y + 1.0);
-    Vec3::unit_vector(1.0).mul(1.0 - t) + Vec3::new(0.5, 0.7, 1.0).mul(t)
+#[derive(Clone, Copy)]
+struct HitRecord {
+    t: f64,
+    p: Vec3,
+    normal: Vec3,
 }
 
-fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> f64 {
-    let oc = r.origin() - center;
-    let a = r.direction().dot(r.direction());
-    let b = 2.0 * oc.dot(r.direction());
-    let c = oc.dot(oc) - radius*radius;
-    let discriminant = b*b - 4.0*a*c;
-    if discriminant < 0.0 {
-        -1.0
+trait Hitable {
+    fn hit(&self, r: Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
+}
+
+#[derive(Clone)]
+struct Sphere {
+    center: Vec3,
+    radius: f64,
+}
+
+impl Sphere {
+    fn new(center: Vec3, radius: f64) -> Self {
+        Sphere { center: center, radius: radius }
+    }
+}
+
+impl Hitable for Sphere {
+    fn hit(&self, r: Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let oc = r.origin() - self.center;
+        let a = r.direction().dot(r.direction());
+        let b = oc.dot(r.direction());
+        let c = oc.dot(oc) - self.radius*self.radius;
+        let discriminant = b*b - a*c;
+        if discriminant > 0.0 {
+            let t1 = (-b - (b*b - a*c).sqrt()) / a;
+            if t1 < t_max && t1 > t_min {
+                let p = r.point_at_parameter(t1);
+                return Some(HitRecord{
+                    t: t1,
+                    p: p,
+                    normal: (p - self.center).div(self.radius),
+                });
+            }
+
+            let t2 = (-b + (b*b - a*c).sqrt()) / a;
+            if t2 < t_max && t2 > t_min {
+                let p = r.point_at_parameter(t2);
+                return Some(HitRecord{
+                    t: t2,
+                    p: p,
+                    normal: (p - self.center).div(self.radius),
+                });
+            }
+        }
+
+        None
+    }
+}
+
+struct HitableList {
+    hitables: Vec<Box<Hitable>>,
+}
+
+impl Hitable for HitableList {
+    fn hit(&self, r: Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let mut closest_so_far = t_max;
+        let mut closest_hit = None;
+        for ref item in &self.hitables {
+            if let Some(hit) = item.hit(r, t_min, closest_so_far) {
+                closest_so_far = hit.t;
+                closest_hit = Some(hit);
+            }
+        }
+        closest_hit
+    }
+}
+
+fn color<T>(r: Ray, world: &T) -> Vec3 where T: Hitable {
+    if let Some(hit) = &world.hit(r, 0.0, f64::MAX) {
+        hit.normal.add(1.0).mul(0.5)
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        let unit_direction = r.direction().make_unit_vector();
+        let t = 0.5 * (unit_direction.y + 1.0);
+        Vec3::unit_vector(1.0).mul(1.0 - t) + Vec3::new(0.5, 0.7, 1.0).mul(t)
     }
 }
-
 
 fn main() {
     let nx = 400;
@@ -152,12 +214,19 @@ fn main() {
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
 
+    let world = HitableList{
+        hitables: vec![
+            Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)),
+            Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.)),
+        ],
+    };
+
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u = i as f64 / nx as f64;
             let v = j as f64 / ny as f64;
             let r = Ray::new(origin, lower_left_corner + horizontal.mul(u) + vertical.mul(v));
-            let col = color(r);
+            let col = color(r, &world);
 
             let col2 = col.mul(255.99);
             println!("{} {} {}", col2.x as i64, col2.y as i64, col2.z as i64);
