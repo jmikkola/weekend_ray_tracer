df2dd7af91d2a2f4169991e9e8124e21f29a8853
Jeremy Mikkola <jeremy@jeremymikkola.com>
2019-02-02 19:55:33 -0800

    Part 1 - test image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let nx = 200;
+    let ny = 100;
+    println!("P3");
+    println!("{} {}", nx, ny);
+    println!("255");
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r = i as f64 / nx as f64;
+            let g = j as f64 / nx as f64;
+            let b = 0.2;
+            let ir = (255.99 * r) as i64;
+            let ig = (255.99 * g) as i64;
+            let ib = (255.99 * b) as i64;
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
