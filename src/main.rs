e8896bc1806c0382a97770a10d313bb616246bd0
Jeremy Mikkola <jeremy@jeremymikkola.com>
2019-02-02 20:52:34 -0800

    Surface normals

--- src/main.rs
+++ src/main.rs
@@ -115,27 +115,33 @@ impl Ray {
 fn color(r: Ray) -> Vec3 {
     let center = Vec3::new(0.0, 0.0, -1.0);
     let radius = 0.5;
-    if hit_sphere(center, radius, r) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(center, radius, r);
+    if t > 0.0 {
+        let n = (r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0)).make_unit_vector();
+        return Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0).mul(0.5);
     }
     let unit_direction = r.direction().make_unit_vector();
     let t = 0.5 * (unit_direction.y + 1.0);
     Vec3::unit_vector(1.0).mul(1.0 - t) + Vec3::new(0.5, 0.7, 1.0).mul(t)
 }
 
-fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f64, r: Ray) -> f64 {
     let oc = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius*radius;
     let discriminant = b*b - 4.0*a*c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 
 fn main() {
-    let nx = 200;
-    let ny = 100;
+    let nx = 400;
+    let ny = 200;
 
     println!("P3");
     println!("{} {}", nx, ny);
